c128b1f2eef54f355332f2fcacd669d9d95441d3
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-10 10:07:43 -0400

    Add encoding to top of python files

--- talk_feed.py
+++ talk_feed.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 A tool for creating podcasts of speakers from General Conference.
 
--- test_database.py
+++ test_database.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import talk_feed as tf
 import database as db
