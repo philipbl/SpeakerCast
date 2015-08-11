ecf3d4db06514fda8c8304c7d9b418252da36da9
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-11 11:09:25 -0400

    Start working on tests

--- test_database.py
+++ test_database.py
@@ -3,11 +3,23 @@
 
 import talk_feed as tf
 import database as db
+import pytest
+
+# @pytest.fixture
+# def db():
+#     pass
 
 
 def test_get_talk():
-    pass
+    talks = db.get_talk("Sterling W. Sill")
+    assert len(talks) == 13
+
+    talks = db.get_talk("Gordon B. Hinckley")
+    assert len(talks) == 210
 
 
 def test_get_all_speaker_and_count():
     pass
+    # for i, (count, speaker) in enumerate(db.get_all_speaker_and_counts()):
+    #     print(i)
+
