c2e06ab5ce571b1715129dcad73052e053066fac
Philip Lundrigan <philiplundrigan@gmail.com>
2017-04-21 20:55:08 -0600

    Add shortcuts to specific app links

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -3,8 +3,14 @@ var speakercastApp = angular.module('speakercastApp', []);
 var first_presidency_names = ["Thomas S. Monson", "Henry B. Eyring", "Dieter F. Uchtdorf"];
 var twelve_apostles_names = ["Russell M. Nelson", "Dallin H. Oaks", "M. Russell Ballard", "Robert D. Hales", "Jeffrey R. Holland", "David A. Bednar", "Quentin L. Cook", "D. Todd Christofferson", "Neil L. Andersen", "Ronald A. Rasband", "Gary E. Stevenson", "Dale G. Renlund"];
 
-// var server_url = "http://127.0.0.1:5000";
-var server_url = "http://speakercast.app.lundrigan.org";
+speakercastApp.config( [
+  '$compileProvider',
+  function( $compileProvider )
+  {
+      $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|itpc|mailto|overcast|castro|downcast|instacast|pcast|chrome-extension):/);
+      // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+  }
+]);
 
 speakercastApp.controller('SpeakerController', ['$scope', '$http', 'orderByFilter', function($scope, $http, orderBy) {
     $scope.first_presidency = [];
@@ -32,12 +38,34 @@ speakercastApp.controller('SpeakerController', ['$scope', '$http', 'orderByFilte
 
       $scope.propertyName = $scope.getLastName;
       $scope.speakers = orderBy(data, $scope.propertyName, false);
+      console.log($scope.propertyName);
 
     });
+
+    $scope.feed = function(speaker) {
+      console.log(speaker);
+      console.log(window.location.hostname);
+      console.log(window.location.pathname);
+
+      var url = window.location.hostname + window.location.pathname + "feeds/" + encodeURIComponent(speaker) + ".rss";
+      console.log(url);
+
+      $scope.castroLink = "castro://subscribe/" + url;
+      $scope.downcastLink = "downcast://" + url;
+      $scope.itunesLink = "itpc://" + url;
+      $scope.podcastsLink = "pcast://" + url;
+      $scope.overcastLink = "overcast://x-callback-url/add?url=" + url;
+      $scope.link = "http://" + url;
+
+      $('#feed-modal').modal({
+        keyboard: false
+      })
+    };
 
     $scope.sortBy = function(propertyName, reverse) {
       $scope.propertyName = propertyName;
       $scope.speakers = orderBy(data, $scope.propertyName, reverse);
+      console.log($scope.propertyName);
     };
 
     $scope.getLastName = function(user) {
